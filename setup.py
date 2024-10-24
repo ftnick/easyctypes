6ccab4c903f778502cf84c301ec278f8366f2789
nick <153327552+ftnick@users.noreply.github.com>
2024-10-25 00:02:24 +1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='easyctypes',
-    version='0.1.2',
+    version='0.1.3',
     packages=find_packages(),
     extras_require={
         'dev': [
